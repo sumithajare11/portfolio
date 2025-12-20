9ea806ceef639425c80a5d5c4496a0927205f42f
Sumit Hajare <sumithajare2003@gmail.com>
2025-12-21 01:14:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Software Engineer","Embedded Developer","Iot & Embedded Engineer","full stack development"],
+    strings: ["Software Engineer","Embedded Developer","Iot & Embedded system","full stack development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
