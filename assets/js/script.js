f66ef7bbf967f9591690e9274cc8a52625d6d906
Sumit Hajare <sumithajare2003@gmail.com>
2026-01-08 18:37:51 +0530

    Refactor Tawk.to Live Chat script integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,19 +191,15 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://tawk.to/chat/695fa7a32f7d05197b28f1c2/1jeeqdmo8';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://tawk.to/chat/695fa7a32f7d05197b28f1c2/1jeeqdmo8';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
 })();
-</script>
-<!--End of Tawk.to Script-->
 // End of Tawk.to Live Chat
 
 
