6103daed2980c92016cbdeed177298daa26bf57b
Sumit Hajare <sumithajare2003@gmail.com>
2026-01-08 18:24:19 +0530

    Replace Tawk.to script with updated version
    Updated Tawk.to Live Chat script with new source URL.

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,15 +191,19 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/695fa7a32f7d05197b28f1c2/1jeeqdmo8';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
 })();
+</script>
+<!--End of Tawk.to Script-->
 // End of Tawk.to Live Chat
 
 
