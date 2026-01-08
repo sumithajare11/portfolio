11cbae67e90f97c0f730cae9fb3a8b2bb30877c6
Sumit Hajare <sumithajare2003@gmail.com>
2026-01-08 18:33:39 +0530

    Update Tawk.to script source URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -197,7 +197,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/695fa7a32f7d05197b28f1c2/1jeeqdmo8';
+s1.src='https://tawk.to/chat/695fa7a32f7d05197b28f1c2/1jeeqdmo8';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
